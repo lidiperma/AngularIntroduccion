75e55ddc8eeae3fe3e082108c9596452e7a0fb5f
lidiperma <lidiperma@gmail.com>
2017-05-24 13:52:37 +0200

    Es para mi repositorio local

--- script8.js
+++ script8.js
@@ -1,25 +1,31 @@
-var app=angular.module("app",[]);
- 
-app.constant("idioma","es-es");
- 
-app.constant("matematicas_simples",{
-  sumar:function(a,b) {
-    return a+b;
-  },
-  restar:function(a,b) {
-    return a-b;
-  }
+var app = angular.module("app", []);
+
+//definimos una constante llamada idioma, y valor "es-es"
+//se define llamando al método constant
+app.constant("idioma", "es-es");
+
+//definimos una constante de nombre "matematicas_simples" que es un objeto Json
+//con dos método , sumar y restar
+app.constant("matematicas_simples", {
+    
+    sumar: function (a, b) {
+        return a + b;
+    },
+    restar: function (a, b) {
+        return a - b;
+    }
 });
- 
-app.constant("radio",10);
- 
-app.constant("area",function(radio) {
-  return 3.1416*radio*radio;
+
+//OJO son constantes que llaman a constantes
+app.constant("radio", 10);
+//Las constantes se pueden inyectar en cualquier sitio
+app.constant("area", function (radio) {
+    return 3.1416 * radio * radio;
 });
- 
-app.controller("PruebaController",["$scope","idioma","matematicas_simples","radio","area",function($scope,idioma,matematicas_simples,radio,area) {
-  $scope.idioma=idioma;
-  $scope.suma=matematicas_simples.sumar(3,6);
-  $scope.area=area(radio);
-}]);
+
+app.controller("PruebaController", ["$scope", "idioma", "matematicas_simples", "radio", "area", function ($scope, idioma, matematicas_simples, radio, area) {
+        $scope.idioma = idioma;
+        $scope.suma = matematicas_simples.sumar(3, 6);
+        $scope.area = area(radio);
+    }]);
 
