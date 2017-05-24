611e59f2d6d1da7927ca754a51e5e40d47491c24
lidiperma <lidiperma@gmail.com>
2017-05-24 13:40:12 +0200

    Es para mi repositorio local

--- script7.js
+++ script7.js
@@ -2,7 +2,7 @@ var app=angular.module("app",[]);
   
 app.controller("PruebaController",['$scope','$timeout',function($scope,$timeout) {
   $scope.producidoEvento="NO";
-  
+  //ejecucion de funciones que se utilizan repetidamente
   $timeout(function() {
     $scope.producidoEvento="SIIIIII";
   },3000);
