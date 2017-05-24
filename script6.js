416a33e178de8b59af1644e9ad580e6caa2eebee
lidiperma <lidiperma@gmail.com>
2017-05-24 13:13:22 +0200

    Es para mi repositorio local

--- script6.js
+++ script6.js
@@ -26,12 +26,17 @@ app.controller("SeguroController",['$scope','$log','$http',function($scope,$log,
   };
    
   $log.debug("Acabamos de crear el $scope");
- 
+  //PETICIÓN POR AJAX
   $http({
     method: 'GET', 
     url: 'datos.json'
+  //funcion de callback de exito
   }).success(function(data, status, headers, config) {
+      //el objeto data contiene los datos (en txt)
       $scope.seguro=data;
+      alert(status);
+      alert(statusText);
+      //funcion de callback de error
   }).error(function(data, status, headers, config) {
       alert("Ha fallado la petición. Estado HTTP:"+status);
   });
